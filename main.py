e3d497396df75204dd118afc755686b9c8a9e20f
zch9241 <2426936965@qq.com>
2023-12-16 23:03:16 +0800

    完成UI界面设计(多线程实现)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # 
 # GNU GENERAL PUBLIC LICENSE Version 3
 # 
-# version: 1.5.alpha
+# version: 1.6
 # 
 # 版本更新说明：
 # v1.0 程序首个版本
@@ -20,101 +20,376 @@
 #              （example: autorun.py 310 True   #循环310次，使用OCR）
 # v1.5.alpha 程序的重构版本（基于pywinauto），减少冗余代码、功能和外部程序调用
 #            更改ln102你的cloudmusic程序路径和ln112循环次数即可使用
-#
+# v1.6 增加UI功能
+# 
+# 
 
-print('<NeteaseCloudMusic> Copyright (C) <2023>  <zch9241>')
-
-from pywinauto.application import Application
 import time
-import pyautogui
 from win10toast import ToastNotifier
+import logging
+import pyautogui
+from io import StringIO
+from functools import wraps
 
-played_list = []
-cur_time,end_time = 0,0
+# 以下两个import会导致PyQt5报错:QWindowsContext: OleInitialize() failed:  "COM error 0xffffffff80010106 RPC_E_CHANGED_MODE (Unknown error 0x080010106)"
+# 或pywinauto报错[WinError -2147417850] 无法在设置线程模式后对其加以更改。
+# 详情: https://www.cnpython.com/qa/628847
+#设置单线程(STA)模式，防止pyqt5与pywinauto冲突
+import sys
+sys.coinit_flags = 2
+from pywinauto.application import Application 
+# 
 
-def executing_time(func):
-    def inner():
-        start = time.time()
-        a,b = func()
-        end = time.time()
-        last = end-start
-        print(executing_time.__name__, func,'执行时间: ', last, 's')
-        return a,b
-    return inner
+from PyQt5.QtCore import QObject, QThread, Qt, pyqtSignal
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+
+import Ui_qt5design
 
 
-@executing_time
-def time_monitor():
-    text = dlg['Document']     #目标控件(只要是print_control_identifiers()方法输出的控件就可直接访问)
-    t = str(text.texts())      #控件文本
 
-    middle_var = t.split(' ')
-    end_time = middle_var[-26]
-    cur_time = middle_var[-35]
+
+
+
+class RunThread(QThread):
+    #设置信号
+    output_ = pyqtSignal(str)
     
-    return cur_time,end_time
-
-def time_manager():
-    cur_time2,cur_time4 = 0,0
-
-    cur_time0,end_time0 = time_monitor()        #独立变量防干扰
-    minute = int(end_time0.split(':')[0])
-    title = dlg.texts()                         #原曲名
-
-    print(time_manager.__name__, '当前曲目:', str(title)[2:-2].replace(r'\xa0', ' '), '总时长: ', end_time0)
-    print(time_manager.__name__, '计数:', len(played_list))
+    def __init__(self, logger: logging.Logger,
+                  cur_time , end_time, 
+                  n, firstrun, played_list, 
+                  playflag
+                    ) -> None:
+        
+        #获取父类的变量
+        self.logger = logger
+        self.cur_time, self.end_time = cur_time,end_time
+        self.n = n
+        self.firstrun = firstrun
+        self.playflag = playflag
+        #
+        self.played_list = played_list
+        
+        super().__init__()
     
-    if minute >= 1:                                                 #时间超过一分钟，需要检测时间来切歌
-        print(time_manager.__name__, '时长超过1min, 启用监测')
+
+    def output_emit_util(self):
+        ## 每次调用都重置output
+        # 创建一个输出到字符串的处理器
+        self.output = StringIO()
+        self.handler = logging.StreamHandler(self.output)
+        self.handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.handler.setFormatter(formatter)
+        self.logger.addHandler(self.handler)
+
+    def output_emit(self):
+        '''
+        将RunThread的log输出至窗口
+        '''
+        output_text = self.output.getvalue()
+        self.output_.emit(output_text)
+
+    #覆写run方法
+    def run(self):
+        #创建进程实例
+        self.app = Application('uia').start(r'D:\Program Files\Netease\CloudMusic\cloudmusic.exe')
+        self.output_emit_util()
+        self.logger.debug('进程实例已创建: %s' % self.app)
+        self.output_emit()
+        #创建窗口实例
+        self.dlg = self.app['Dialog']     
+        time.sleep(5)           #等待窗口加载完成
+        #dlg.print_control_identifiers()
+
+        pyautogui.hotkey('ctrl', 'alt', 'p')
+
+        for i in range(self.n):
+            self.time_manager()
+
+        pyautogui.hotkey('ctrl', 'alt', 'p')
+
+        #print(played_list)
+
+        ToastNotifier().show_toast('notification', '程序运行完成', '.\\icon\\py.ico')
+
+
+
+    def executing_time(func):
+        @wraps(func)
+        def inner(self, *args, **kwargs):
+            start = time.time()
+            a,b = func(self, *args, **kwargs)
+            end = time.time()
+            last = end-start
+            self.output_emit_util()
+            self.logger.debug(
+                self.executing_time.__name__ + str(func) 
+                + '执行时间: ' + str(round(last, 3)) + 's'
+                )
+            self.output_emit()
+            return a,b
+        return inner
+
+
+    @executing_time
+    def time_monitor(self):
+        self.text = self.dlg['Document']     #目标控件(只要是print_control_identifiers()方法输出的控件就可直接访问)
+        self.t = str(self.text.texts())      #控件文本
+
+        self.middle_var = self.t.split(' ')
+        self.end_time = self.middle_var[-26]
+        self.cur_time = self.middle_var[-35]
+
+        return self.cur_time,self.end_time
+
+    def time_manager(self):
+        self.cur_time2,self.cur_time4 = 0,0
+
+        self.cur_time0,self.end_time0 = self.time_monitor()        #独立变量防干扰
+        self.minute = int(self.end_time0.split(':')[0])
+        self.title = self.dlg.texts()                         #原曲名
+
+        self.output_emit_util()
+        self.logger.info(self.time_manager.__name__ +
+                         '当前曲目:' + str(self.title)[2:-2].replace(r'\xa0', ' ') + 
+                         '总时长: ' + self.end_time0)
+        self.output_emit()
+        
+        self.output_emit_util()
+        self.logger.info(self.time_manager.__name__ + 
+                         '计数:' + str(len(self.played_list)))
+        self.output_emit()
+
+        if self.minute >= 1:                                                 #时间超过一分钟，需要检测时间来切歌
+            self.output_emit_util()
+            self.logger.info(self.time_manager.__name__ + '时长超过1min, 启用监测')
+            self.output_emit()
+            while True:
+                self.cur_time1,self.end_time1 = self.time_monitor()
+                self.title1 = self.dlg.texts()                                    #当前曲名
+                
+                self.output_emit_util()
+                self.logger.debug(self.time_manager.__name__ + '当前时间: %s' % self.cur_time1)
+                self.output_emit()
+
+
+                if self.title1 != self.title:
+                    self.output_emit_util()
+                    self.logger.warning(self.time_manager.__name__ + '侦测到计划外的切歌')
+                    self.output_emit()
+
+                    self.played_list.append(self.title)
+                    
+                    self.output_emit_util()
+                    self.logger.warning(self.time_manager.__name__ + '结束当前循环...')
+                    self.output_emit()
+                    
+                    break
+                elif self.cur_time1 == self.cur_time2 and self.playflag == True:
+                    self.output_emit_util()
+                    self.logger.warning(self.time_manager.__name__ + '侦测到计划外的停止, 再次播放...')
+                    self.output_emit()
+                    
+                    pyautogui.hotkey('ctrl', 'alt', 'p')
+                elif int(self.cur_time1.split(':')[0]) >= 1:
+                    self.played_list.append(self.title)
+                    
+                    self.output_emit_util()
+                    self.logger.debug(self.time_manager.__name__ + '切歌...')
+                    self.output_emit()
+                    
+                    pyautogui.hotkey('ctrl', 'alt', 'right')
+                    break
+                self.cur_time2,self.end_time2 = self.time_monitor()
+        else:
+            self.output_emit_util()
+            self.logger.info(self.time_manager.__name__ + '时长不足1min, 禁用监测')
+            self.output_emit()
+            
+            while True:
+                self.cur_time3,self.end_time3 = self.time_monitor()
+                self.title2 = self.dlg.texts()
+                if self.title2 != self.title:
+                    self.played_list.append(self.title)
+                    
+                    self.output_emit_util()
+                    self.logger.debug(self.time_manager.__name__ + '较短的歌曲已播放结束, 结束当前循环...')
+                    self.output_emit()
+                    
+                    break
+                elif self.cur_time3 == self.cur_time4 and self.playflag == True:
+                    self.output_emit_util()
+                    self.logger.warning(self.time_manager.__name__ + '侦测到计划外的停止, 再次播放...')
+                    self.output_emit()
+                    
+                    pyautogui.hotkey('ctrl', 'alt', 'p')
+                self.cur_time4,self.end_time4 = self.time_monitor()
+                time.sleep(2)
+
+
+
+class MainWin(QWidget):
+    def __init__(self):
+        self.cur_time, self.end_time = 0,0
+        self.n = 0
+        self.firstrun = True
+        self.playflag = True
+        self.played_list = []
+
+        super().__init__()
+        self.init_logger()
+        self.init_ui()
+    
+    def init_logger(self):
+        '''
+        日志相关
+        '''
+        # 创建一个日志记录器
+        self.logger = logging.getLogger(' ' + __name__ + ' ')
+        self.logger.setLevel(logging.DEBUG)
+        #
+        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        self.handler_ = logging.StreamHandler()
+        self.handler_.setFormatter(self.formatter)
+        
+        self.logger.addHandler(self.handler_)
+
+    def init_ui(self):
+        '''
+        UI初始化设置
+        '''
+        self.ui = Ui_qt5design.Ui_MainWindow()
+        self.window = QMainWindow()
+        self.ui.setupUi(self.window)
+
+        #信号绑定槽函数
+        self.ui.pushButtonRun.clicked.connect(self.pushButtonRunfunc)
+        self.ui.pushButtonStop.clicked.connect(self.pushButtonStop)
+        self.ui.horizontalSliderCount.sliderMoved.connect(self.Scountreader)
+        self.ui.lineEditCount.editingFinished.connect(self.Lcountreader)
+
+
+
+    def output_to_window(self, text):
+        '''
+        打印输出(更新窗口文本)
+        '''
+        originaltext = self.ui.textBrowserLogger.toPlainText()
+
+        self.ui.textBrowserLogger.setText(originaltext + text)
+    
+    
+    def outputter_util(self):
+        '''
+        先调用util创建handler,执行完.log()语句后再调用outputter---可用装饰器，但太麻烦
+        '''
+        ## 每次调用都重置output
+        # 创建一个输出到字符串的处理器
+        self.output = StringIO()
+        self.handler_ = logging.StreamHandler(self.output)
+        self.handler_.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler_)
+    def outputter(self):
+        '''
+        将MainWin的log输出至窗口
+        '''
+        self.output_text = self.output.getvalue()
+        self.output_to_window(self.output_text)
+    
+    def outputter2(self, text):
+        '''
+        接受RunThread的singal(output_)并将其log输出至窗口
+        '''
+        self.output_to_window(text)
+
+    def run_thread(self):
+        '''
+        子线程启动入口
+        '''
+        self.thread = RunThread(self.logger, self.cur_time, self.end_time,
+                                self.n, self.firstrun, self.played_list, 
+                                self.playflag)
+        #启动线程
+        self.thread.start()
+        self.thread.output_.connect(self.outputter2)
+
+
+
+    def pause(self):
+        self.runflag = True
         while True:
-            cur_time1,end_time1 = time_monitor()
-            title1 = dlg.texts()                                    #当前曲名
-            print(time_manager.__name__, '当前时间: %s' % cur_time1)
+            if self.runflag == True:
+                time.sleep(0.1)
+            else:
+                self.playflag = False
+                
+                self.outputter_util()
+                self.logger.info(self.pause.__name__ + ' 已暂停 \n')
+                self.outputter()
 
-            if title1 != title:
-                print(time_manager.__name__, '侦测到计划外的切歌')
-                played_list.append(title)
-                print(time_manager.__name__, '结束当前循环...')
-                break
-            elif cur_time1 == cur_time2:
-                print(time_manager.__name__, '侦测到计划外的停止, 再次播放...')
                 pyautogui.hotkey('ctrl', 'alt', 'p')
-            elif int(cur_time1.split(':')[0]) >= 1:
-                played_list.append(title)
-                print(time_manager.__name__, '切歌...')
-                pyautogui.hotkey('ctrl', 'alt', 'right')
-                break
-            cur_time2,end_time2 = time_monitor()
-    else:
-        print(time_manager.__name__, '时长不足1min, 禁用监测')
-        while True:
-            cur_time3,end_time3 = time_monitor()
-            title2 = dlg.texts()
-            if title2 != title:
-                played_list.append(title)
-                print(time_manager.__name__, '较短的歌曲已播放结束, 结束当前循环...')
-                break
-            elif cur_time3 == cur_time4:
-                print(time_manager.__name__, '侦测到计划外的停止, 再次播放...')
+                while self.runflag == False:
+                    time.sleep(0.1)
+                
+                self.playflag = True
+                self.echoflag = True
+                
+                self.outputter_util()
+                self.logger.info(self.pause.__name__ + ' 已恢复 \n')
+                self.outputter()
+
                 pyautogui.hotkey('ctrl', 'alt', 'p')
-            cur_time4,end_time4 = time_monitor()
-            time.sleep(2)
 
 
-#创建进程实例
-app = Application('uia').start(r'D:\Program Files\Netease\CloudMusic\cloudmusic.exe')
-print('进程实例已创建: %s' % app)
+    #---------------------------槽函数----
+    def pushButtonRunfunc(self):
+        self.outputter_util()
+        self.logger.debug('恢复')
+        self.outputter()
+        self.runflag = True
+        if self.firstrun == True:
+            self.logger.info(self.pushButtonRunfunc.__name__ + ' 启动程序 \n')
+            self.run_thread()
+            self.firstrun = False
+        self.ui.pushButtonRun.setDisabled(True)
+        self.ui.pushButtonStop.setEnabled(True)
+    def pushButtonStop(self):
+        self.outputter_util()
+        self.logger.info(self.pushButtonStop.__name__ + ' 暂停程序 \n')
+        self.outputter()
+       
+        self.runflag = False
+        self.ui.pushButtonStop.setDisabled(True)
+        self.ui.pushButtonRun.setEnabled(True)
+    def Scountreader(self):
+        self.ui.pushButtonRun.setEnabled(True)
+        self.n = self.ui.horizontalSliderCount.value()
+        self.ui.lineEditCount.setText(str(self.n))
+        
+        self.outputter_util()
+        self.logger.debug(self.n)
+        self.outputter()
+    def Lcountreader(self):
+        self.ui.pushButtonRun.setEnabled(True)
+        try:
+            self.n = int(self.ui.lineEditCount.text())
+            self.ui.horizontalSliderCount.setValue(self.n)
+            self.outputter_util()
+            self.logger.debug(self.n)
+            self.outputter()
+        except ValueError:
+            self.outputter_util()
+            self.logger.warn(self.Lcountreader.__name__, " 输入的值有误 value = '%s'" % self.ui.lineEditCount.text())
+            self.outputter()
+    #---------------------------槽函数----
 
-#创建窗口实例
-dlg = app['Dialog']     
-time.sleep(5)           #等待窗口加载完成
-#dlg.print_control_identifiers()
 
-pyautogui.hotkey('ctrl', 'alt', 'p')
 
-for i in range(10):
-    time_manager()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainwin = MainWin()
+    mainwin.window.show()
+    sys.exit(app.exec_())
 
-pyautogui.hotkey('ctrl', 'alt', 'p')
-#print(played_list)
-ToastNotifier().show_toast('notification', '程序运行完成', '.\\icon\\py.ico')
+    
--- tests/t.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-import time
-#from pywinauto.application import Application
-import pyautogui
-from win10toast import ToastNotifier
-import threading
-import logging
-app = QtWidgets.QApplication(sys.argv)
